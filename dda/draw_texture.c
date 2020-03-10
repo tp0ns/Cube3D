dd8bbc2b9707ddf84b07a558f196a608450e91a1
tpons@student.42.fr <tpons@student.42.fr>
2020-03-10 13:58:09 +0100

    Improvement texture

--- dda/draw_texture.c
+++ dda/draw_texture.c
@@ -6,7 +6,7 @@
 /*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/05 14:03:03 by tpons             #+#    #+#             */
-/*   Updated: 2020/03/10 13:48:49 by tpons            ###   ########.fr       */
+/*   Updated: 2020/03/10 13:57:50 by tpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,18 +36,19 @@ void	set_texture(t_param *p)
 	if (p->d->side == 1 || p->d->side == 2)
 	{
 		p->d->wallx = p->s->pos_y + p->d->walldist * p->d->raydiry;
-		p->d->textx = p->d->wallx * (double)p->i[p->d->side]->width;
+		p->d->wallx -= floor(p->d->wallx);
+		p->d->textx = (int)(p->d->wallx * (double)p->i[p->d->side]->width);
 		if (p->d->raydirx > 0)
 			p->d->textx = p->i[p->d->side]->width - p->d->textx - 1;
 	}
 	else if (p->d->side == 3 || p->d->side == 4)
 	{
 		p->d->wallx = p->s->pos_x + p->d->walldist * p->d->raydirx;
-		p->d->textx = p->d->wallx * (double)p->i[p->d->side]->width;
+		p->d->wallx -= floor(p->d->wallx);
+		p->d->textx = (int)(p->d->wallx * (double)p->i[p->d->side]->width);
 		if (p->d->raydiry < 0)
 			p->d->textx = p->i[p->d->side]->width - p->d->textx - 1;
 	}
-	p->d->wallx -= floor(p->d->wallx);
 }
 
 int		draw_textures(t_param *p, int y)
