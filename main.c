1ab9c97c24f66606a77967ed7d38380841073d87
tpons@student.42.fr <tpons@student.42.fr>
2020-03-10 15:23:43 +0100

    Check file name

--- main.c
+++ main.c
@@ -6,11 +6,22 @@
 /*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/21 13:39:10 by tpons             #+#    #+#             */
-/*   Updated: 2020/03/05 17:23:14 by tpons            ###   ########.fr       */
+/*   Updated: 2020/03/10 15:21:19 by tpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
+
+int	file_name(char *str)
+{
+	int	len;
+
+	len = ft_strlen(str) - 1;
+	if (str[len] == 'b' && str[len - 1] == 'u' && str[len - 2] == 'c'
+	&& str[len - 3] == '.')
+		return (1);
+	return (0);
+}
 
 int	main(int ac, char **av)
 {
@@ -18,7 +29,7 @@ int	main(int ac, char **av)
 	t_param		p;
 
 	fd = 0;
-	if (ac == 2)
+	if (ac == 2 && file_name(av[1]))
 	{
 		fd = open(av[1], O_RDONLY);
 		init_param(&p);
@@ -26,7 +37,9 @@ int	main(int ac, char **av)
 		get_param(fd, &p);
 		game(&p);
 	}
-	// if (ac == 3) Pour Screenshot;
+	// else if (ac == 3 && file_name(av[1])) Pour Screenshot;
+	else
+		leave("You must use .cub extension for your scene file");
 	close(fd);
 	return (0);
 }
